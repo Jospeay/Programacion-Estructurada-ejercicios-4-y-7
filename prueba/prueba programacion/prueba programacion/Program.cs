b953d86a5553a7aa37ec0d56db59cc3b041f102f
Diego052508 <dfarmengol@uamv.edu.ni>
2025-09-19 08:33:45 -0600

    ejercicio 7

---
+++ prueba/prueba programacion/prueba programacion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_programacion
+{
+    internal class Program
+    {
+        static void Main()
+        {  
+            while (true)
+            {
+                Console.Write("Ingrese su edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                Console.WriteLine(ClasificarEdad(edad));
+            }
+        }
+        static string ClasificarEdad(int edad)
+        {
+            if (edad <= 0) return "ERROR: edad inválida";
+            else if (edad < 18) return "Menor de edad";
+            else if (edad >= 18 && edad <= 60) return "Persona adulta";
+            else return "Persona adulto mayor";
+        }
+    }
+    }
+    
+
