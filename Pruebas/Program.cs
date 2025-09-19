d0582b6f4259338ff1938787cf290eb7d8312494
g-nzaan <asgmshift@gmail.com>
2025-09-19 08:32:42 -0600

    Ejercicio #4
    Lesther, te quiero

---
+++ Pruebas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class vocales
+{
+    static int ContarVocales(string oracion)
+    {
+        int contador = 0;
+        foreach (char vocal in oracion.ToLower())
+        {
+            if ("aeiou".Contains(vocal)) contador++;
+        }
+        return contador;
+    }
+    static void Main()
+    {
+        Console.Write("Ingrese una cadena de texto: ");
+        string cadena = Console.ReadLine();
+
+        Console.WriteLine($"Cantidad de vocales: {ContarVocales(cadena)}");
+    }
+}
